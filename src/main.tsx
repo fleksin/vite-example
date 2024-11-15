1ee91b414463e2eff8a9b282ae4be4e5efa6f0cd
fleksin <hezhoufan@gmail.com>
2024-11-15 17:36:05 +0800

    save for now

---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import { TomoWalletTgSdkV2, initTomoModal, CONNECT_MAP } from '@tomo-inc/tomo-telegram-sdk'
+import '@tomo-inc/tomo-telegram-sdk/dist/styles.css'
+new TomoWalletTgSdkV2({ injected: true });
+async function init() {
+  await initTomoModal({
+    onConnect: (wall: any) => {
+      console.log(wall);
+    },
+    supportedProviders: ['TON'],
+    supportedConnects: [
+      CONNECT_MAP.TOMO_MINI_APP,
+      CONNECT_MAP.OKX_CONNECT,
+      CONNECT_MAP.TON_CONNECT,
+    ],
+    manifestUrl: 'https://d8o5s6z018yzr.cloudfront.net/manifestUrl.json',
+  });
+}
+init();
+
+
+
+
+
---
+++ src/vite-env.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="vite/client" />
+declare module "@ton/ton";
+declare module "@tomo-inc/tomo-telegram-sdk";
